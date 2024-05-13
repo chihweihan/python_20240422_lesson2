9cc5491cda3e7ee264af474a0fdb658b78315918
chihweihan <167328031+chihweihan@users.noreply.github.com>
2024-05-13 11:49:02 +0000

    0513

---
+++ lesson8/lesson8_1.py
@@ -0,0 +1,26 @@
+import pyinputplus as pyip
+import random
+
+min = 1
+max = 100
+count = 0
+target = random.randint(min,max)
+print("==============猜數字遊戲==============\n")
+while True:
+    count += 1
+    keyin = pyip.inputInt(f"猜數字範圍{min}~{max}:")
+    print(keyin)
+    if keyin == target:
+        print(f"賓果！猜對了，答案是：{target}")
+        print(F"您粽共猜了：{count}次")
+        break
+    elif(keyin > target):
+        print("猜錯囉！再小一點")
+        max = keyin - 1
+        print(f"您已經猜了{count}次")
+    elif(keyin < target):
+        print("猜錯囉！再大一點")
+        min = keyin + 1
+    print(f"您已經猜了{count}次")
+
+print("遊戲結束")
